33fbb27a32ad9aa3dec14ed19d89541a62e5f0f2
Chiara <c.stabile15@studenti.unisa.it>
2026-05-29 13:37:44 +0200

    modifiche navigator

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 // App.tsx (nella cartella principale del progetto)
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import AngeloNavigator from './src/navigation/AppNavigator';
+import AppNavigator from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
     // Il NavigationContainer DEVE stare qui, alla radice di tutto (Slide pag. 8)
     <NavigationContainer>
-      <AngeloNavigator />
+      <AppNavigator />
     </NavigationContainer>
   );
 }
